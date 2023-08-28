212220af4d8d2ae8ccd778c313e2e77d7f0dffeb
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-08-28 18:06:54 -0500

    Create tv.py

---
+++ televisores/tv.py
@@ -0,0 +1,75 @@
+class TV:
+
+    numTV = 0
+
+    def __init__(self, marca, estado):
+        self.marca = marca
+        self.canal = 1
+        self.precio = 500
+        self.estado = estado
+        self.volumen = 1
+        self.control = None
+        numTV = numTV + 1
+    
+    def setNumtv(self, num):
+        numTV = num
+
+    def getNumtv(self):
+        return self.numTV
+
+    def setMarca(self, brand):
+        self.marca = brand
+
+    def getMarca(self):
+        return self.marca
+
+    def setCanal(self, chanel):
+        self.canal = chanel
+
+    def getCanal(self):
+        return self.canal
+
+    def setPrecio(self, price):
+        self.precio = price
+
+    def getPrecio(self):
+        return self.precio
+
+    def setVolumen(self, vol):
+        self.volumen = vol
+
+    def getVolumen(self):
+        return self.volumen
+
+    def setControl(self, new):
+        self.control = new
+
+    def getControl(self):
+        return self.control
+    
+    def turnOn(self):
+        if self.estado == False:
+            self.estado = True
+        
+    def turnOff(self):
+        if self.estado == True:
+            self.estado = False
+
+    def setEstado(self):
+        return self.estado
+    
+    def canalDown(self):
+        if self.estado and 1 < self.control <= 120:
+            self.canal -= 1
+
+    def canalUp(self):
+        if self.estado and 1 <= self.control < 120:
+            self.canal += 1
+
+    def volumenDown(self):
+        if self.estado and 0 < self.volumen <= 7:
+            self.volumen -= 1
+
+    def volumenUp(self):
+        if self.estado and 0 <= self.volumen < 7:
+            self.volumen += 1
