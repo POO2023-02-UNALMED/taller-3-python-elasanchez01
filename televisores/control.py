129c0d39930a73e8b1a9c3cdd3745cad881c4637
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-08-28 18:04:30 -0500

    Update and rename code to control.py

---
+++ televisores/control.py
@@ -0,0 +1,31 @@
+class Control:
+
+    def __init__(self, tv):
+        self.tv = tv
+
+    def turnON(self):
+        self.tv.turnOn()
+
+    def turnOff(self):
+        self.tv.turnOff()
+    
+    def canalUp(self):
+        self.tv.canalUp()
+
+    def canalDown(self):
+        self.tv.canalDown()
+
+    def setCanal(self):
+        return self.tv.canal
+
+    def setVolumen(self):
+        return self.tv.volumen
+    
+    def enlazar(self, tv):
+        self.tv = tv
+
+    def setTv(self, tv):
+        self.tv = tv
+
+    def getTv(self):
+        return self.tv
